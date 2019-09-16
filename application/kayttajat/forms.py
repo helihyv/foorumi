188d43bc2c3387728152ad2aec2e322a21b48120
Heli Hyvättinen <heli.hyvattinen@netti.fi>
2019-09-16 19:23:12 +0300

    Käyttäjän lisääminen käyttää flask-bootstrapin WTForm-tukea
    Käyttäjän lisäämisen lomake renderöidään wtf.quick_form -funktiolla. Lomakkeen lähetysnappi sisällytetty WTForm-lomakkeeseen.

--- application/kayttajat/forms.py
+++ application/kayttajat/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField
 
 class KayttajaLomake(FlaskForm):
     nimi = StringField("Käyttäjän nimi")
     tunnus = StringField("Käyttäjätunnus")
     salasana = PasswordField("Salasana")
+    nappi = SubmitField("Luo käyttäjätunnus")
 
     class Meta:
         csrf = False
