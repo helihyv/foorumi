739389d17cc8f59517c8b50e9c91df1e7138e4a9
Heli Hyvättinen <heli.hyvattinen@netti.fi>
2019-09-16 19:32:20 +0300

    Viestin lähettämisen lomake käyttää flask-bootstrapin WTForm-tukea
    Viestin lähettämisen lomake renderöidään flask-bootstrapin quick_form -funktiolla. Lomakkeen lähettämisnappi sisällytetty WTForm-lomakkeeseen.

--- application/viestit/forms.py
+++ application/viestit/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectMultipleField, TextAreaField
+from wtforms import StringField, SelectMultipleField, TextAreaField, SubmitField
 
 class ViestiLomake(FlaskForm):
     otsikko = StringField("otsikko")
     teksti = TextAreaField("teksti")
     aiheet = SelectMultipleField("aihe", coerce=int) 
+    nappi = SubmitField("Lisää viesti")
 
     class Meta:
         csrf = False
