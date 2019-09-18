c75b7389b918bb5913d696bf8ab1067aacc474fd
Heli Hyvättinen <heli.hyvattinen@netti.fi>
2019-09-18 15:41:57 +0300

    Korjattu kirjoitusvirhe __init__.py:ssä

--- application/__init__.py
+++ application/__init__.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 if os.environ.get("HEROKU"):
-    app.config["SQLALCHEMY_DATABASE_URI"] = OS.ENVIRON.GET("DATABASE_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 else:
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///forum.db"
     app.config["SQLALCHEMY_ECHO"] = True
