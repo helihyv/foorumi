509e6746f0f819c7b898506871f55db0c94e3585
Heli Hyvättinen <heli.hyvattinen@netti.fi>
2019-09-18 19:29:37 +0300

    Ryhmän luonti ja jäsenten lisäys: käytössä flask-bootstrapin WTForm-tuki
    Ryhmän luonnin ja jäsenten ryhmään lisäämisen lomakkeet renderöidään flask-bootstrapin quickForm:illa. Lähetysnapit mukana WTForm-lomakkeissa.

--- application/ryhmat/forms.py
+++ application/ryhmat/forms.py
@@ -1,15 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectMultipleField, validators
+from wtforms import StringField, SelectMultipleField, validators, SubmitField
 from application.suomennokset import pituus_validaatiovirheviesti
 
 class RyhmaLomake(FlaskForm):
     nimi = StringField("Ryhmän nimi", [validators.Length(min=4,max=100, message=pituus_validaatiovirheviesti)])
+    nappi = SubmitField("Lisää ryhmä")
 
     class Meta:
         csrf = False
 
 class LisaaJasenLomake(FlaskForm):
     jasenet = SelectMultipleField("uudet jäsenet", coerce=int)
+    nappi = SubmitField("Lisää jäsen")
 
     class Meta:
         csrf = False
