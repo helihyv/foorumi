6addb535e78ce30e02ee042c121e9ccf2465ecc2
Heli Hyvättinen <heli.hyvattinen@netti.fi>
2019-09-18 18:02:08 +0300

    Aiheen lisääminen käyttää flask-bootstrapin WTForm-tukea
    Aiheen lisäämisen lomake renderöidään wtf.quickForm -funktiolla. Lähetysnappi sisällytetty WTForm-lomakkeeseen.

--- application/aiheet/forms.py
+++ application/aiheet/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators
+from wtforms import StringField, validators, SubmitField
 from application.suomennokset import pituus_validaatiovirheviesti
 
 class AiheLomake(FlaskForm):
     aihe = StringField("Aihe", [validators.Length(min=4, max=100, message=pituus_validaatiovirheviesti)])
+    nappi = SubmitField("Lisää aihe")
 
     class Meta:
         csrf = False
