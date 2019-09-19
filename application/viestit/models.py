33fcf9132ad51fc13119bffa5d43c89ae250dc95
Heli Hyvättinen <heli.hyvattinen@netti.fi>
2019-09-19 10:37:12 +0300

    Liitostaulu lukenut-tiedolle
    Liitostaulu Viesti- ja Kayttaja -taulujen välille. Tallentaa tiedon siitä mitkä käyttäjät ovat lukeneet mitkäkin viestit.

--- application/viestit/models.py
+++ application/viestit/models.py
@@ -1,4 +1,10 @@
 from application import db
+from application.kayttajat.models import Kayttaja
+
+luetut = db.Table("luetut",
+db.Column("viesti_id", db.Integer, db.ForeignKey("viesti.id"), primary_key=True),
+db.Column("lukija_id", db.Integer, db.ForeignKey("kayttaja.id"), primary_key=True)
+)
 
 class Viesti(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,6 +14,8 @@ class Viesti(db.Model):
     kirjoittaja_id = db.Column(db.Integer, db.ForeignKey('kayttaja.id'), nullable=False)
     vastattu_id = db.Column(db.Integer, db.ForeignKey("viesti.id"), nullable=True)
     vastaukset = db.relationship("Viesti", backref=db.backref("vastattu", remote_side=[id]))
+    lukeneet = db.relationship(Kayttaja, secondary=luetut, lazy=True,
+    backref=db.backref("luetut_viestit", lazy=True))
 
     def __init__(self, otsikko, teksti, aiheet, vastattu_id, kirjoittanut_kayttaja_id):
         self.otsikko = otsikko
