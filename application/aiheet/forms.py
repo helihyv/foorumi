13109cce0a47be258637aa55fef0f04741363438
Heli Hyvättinen <heli.hyvattinen@netti.fi>
2019-10-16 13:10:36 +0300

    Samannimiset aiheet estetty aiheen lisäämisessä ja muokkaamisessa
    Aiheen lisäämisen ja muokkaamisen pohjalomakkeen validointiin on lisätty funktio, joka tarkastaa, ettei samannimistä aihetta ole jo olemassa.

--- application/aiheet/forms.py
+++ application/aiheet/forms.py
@@ -1,9 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators, SubmitField, HiddenField
+from wtforms import StringField, validators, SubmitField, HiddenField, ValidationError
 from application.suomennokset import pituus_validaatiovirheviesti
+from application.aiheet.models import Aihe
+def validoiUniikkiAihe(form, field):
+    aihe = Aihe.query.filter_by(aihe = field.data).first()
+    if aihe:
+        raise ValidationError("Aihe on jo olemassa")
 
 class AihePohjaLomake(FlaskForm):
-    aihe = StringField("Aihe", [validators.Length(min=4, max=100, message=pituus_validaatiovirheviesti)])
+    aihe = StringField("Aihe", [
+        validators.Length(min=4, max=100, message=pituus_validaatiovirheviesti), 
+        validoiUniikkiAihe
+    ])
     palattava_sivu = HiddenField()
 
     class Meta:
